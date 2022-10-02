19b3ab91142c45beea3a46a5b10bda08b4f7b328
Matthew <102716158+MatthewPerkins6969@users.noreply.github.com>
2022-10-01 19:41:29 -0700

    Create Util.cs

---
+++ Util.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace GreaterThenLessThenUtil
+{
+    public class Utils
+    {
+        public static void GreaterThen(int first, int second)
+        {
+            Console.WriteLine(first > second ? "Second nimber is bigger !!" : "Second number is not bigger !!");
+        }
+        public static void LessThen(int first, int second)
+        {
+            Console.WriteLine(first < second ? "second number is less then !!" : "second number is not less then !!");
+        }
+        public static void EqualTo(int first, int second)
+        {
+            Console.WriteLine(first == second
+                ? "number 1 is equal to number 2 !!"
+                : "number 1 and number 2 are not the same...");
+        }
+    }
+}
