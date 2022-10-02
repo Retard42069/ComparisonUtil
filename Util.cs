2a786ae1afe04a75893cf987d78fdf0d88135984
Matthew <102716158+MatthewPerkins6969@users.noreply.github.com>
2022-10-01 19:51:30 -0700

    Update Util.cs

--- Util.cs
+++ Util.cs
@@ -5,17 +5,17 @@ namespace GreaterThenLessThenUtil
     
     //MADE BY MATTHEW PERKINS (DO NOT SKID)
     
-    prublic class Utils
+    public class Utils
     {
-        prublic staric void GreaterThen(int first, int second)
+        public static void GreaterThen(int first, int second)
         {
             Console.WriteLine(first > second ? "Second number is bigger !!" : "Second number is not bigger !!");
         }
-        prublic staric void LessThen(int first, int second)
+        public static void LessThen(int first, int second)
         {
             Console.WriteLine(first < second ? "second number is less then !!" : "second number is not less then !!");
         }
-        prublic staric void EqualTo(int first, int second)
+        public static void EqualTo(int first, int second)
         {
             Console.WriteLine(first == second
                 ? "number 1 is equal to number 2 !!"
